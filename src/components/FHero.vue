08d3f78ed7b8cc2e5ac6897191da7e295fe60984
Nick Gronow <nick@wov.io>
2020-07-15 08:33:38 -0400

    Added a sqiggle

--- src/components/FHero.vue
+++ src/components/FHero.vue
@@ -3,7 +3,7 @@
 
   .hidden(class="sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full")
     .relative.h-full.max-w-screen-xl.mx-auto
-      svg.absolute.right-full.transform.text-gray-200.rotate-5(width="2000" height="2000" fill="none")
+      svg.absolute.inset-0.transform.text-gray-200.top-0(class="rotate-5 translate-x-1-20 translate-y-1-20 scale-75" width="2000" height="2000" fill="none")
         defs
           pattern(id="cross" x="0" y="0" width="0.22" height="0.15")
             line(x1="18" y1="3" x2="36" y2="48" stroke="currentColor" stroke-width="4")
@@ -11,6 +11,17 @@
             line(x1="218" y1="103" x2="236" y2="148" stroke="currentColor" stroke-width="4")
             line(x1="207" y1="125" x2="242" y2="112" stroke="currentColor" stroke-width="4")
         rect(width="2000" height="2000" fill="url(#cross)")
+
+      svg.absolute.inset-0.transform.text-gray-200.top-0(class="rotate-10 translate-x-1-30 translate-y-1-30 scale-50" width="4000" height="4000" fill="none")
+        defs
+          pattern(id="sqiggle" x="0" y="0" width="0.25" height="0.12")
+            path(d="M 105 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+            path(d="M 125 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+            path(d="M 145 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+            path(d="M 165 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+            path(d="M 185 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+            path(d="M 205 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+        rect(width="4000" height="4000" fill="url(#sqiggle)")
 
   .relative.pt-6.pb-12(class="sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32")
     main.mt-10.mx-auto.max-w-screen-xl.px-4(class="sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28")
