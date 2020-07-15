8a2b022a6d5a6427c63697271cc67ce5467817e4
Nick Gronow <nick@wov.io>
2020-07-15 18:43:02 -0400

    Tweak hero

--- src/components/FHero.vue
+++ src/components/FHero.vue
@@ -3,24 +3,40 @@
 
   .hidden(class="sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full")
     .relative.h-full.max-w-screen-xl.mx-auto
-      svg.absolute.inset-0.transform.text-gray-200.top-0(class="rotate-5 translate-x-1-20 translate-y-1-20 scale-75" width="2000" height="2000" fill="none")
+      svg.absolute.inset-0.transform.text-gray-200(class="rotate-5 translate-x-1-29 translate-y-1-21 scale-75" width="4000" height="4000" fill="none")
         defs
-          pattern(id="cross" x="0" y="0" width="0.22" height="0.15")
-            line(x1="18" y1="3" x2="36" y2="48" stroke="currentColor" stroke-width="4")
-            line(x1="7" y1="25" x2="42" y2="12" stroke="currentColor" stroke-width="4")
-            line(x1="218" y1="103" x2="236" y2="148" stroke="currentColor" stroke-width="4")
-            line(x1="207" y1="125" x2="242" y2="112" stroke="currentColor" stroke-width="4")
-        rect(width="2000" height="2000" fill="url(#cross)")
+          pattern(id="cross" x="0" y="0" width="0.15" height="0.15")
+            line(x1="18" y1="3" x2="36" y2="48" stroke="currentColor" stroke-width="6")
+            line(x1="7" y1="25" x2="42" y2="12" stroke="currentColor" stroke-width="6")
+        rect(width="4000" height="4000" fill="url(#cross)")
 
-      svg.absolute.inset-0.transform.text-gray-200.top-0(class="rotate-10 translate-x-1-30 translate-y-1-30 scale-50" width="4000" height="4000" fill="none")
+      svg.absolute.inset-0.transform.text-gray-200(class="-rotate-5 translate-x-1-22 translate-y-1-27 scale-75" width="4000" height="4000" fill="none")
         defs
-          pattern(id="sqiggle" x="0" y="0" width="0.25" height="0.12")
-            path(d="M 105 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
-            path(d="M 125 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
-            path(d="M 145 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
-            path(d="M 165 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
-            path(d="M 185 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
-            path(d="M 205 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5")
+          pattern(id="cross" x="0" y="0" width="0.15" height="0.15")
+            line(x1="18" y1="3" x2="36" y2="48" stroke="currentColor" stroke-width="6")
+            line(x1="7" y1="25" x2="42" y2="12" stroke="currentColor" stroke-width="6")
+        rect(width="4000" height="4000" fill="url(#cross)")
+
+      svg.absolute.inset-0.transform.text-gray-200(class="rotate-10 translate-x-1-24 translate-y-1-24 scale-75" width="4000" height="4000" fill="none")
+        defs
+          pattern(id="sqiggle" x="0" y="0" width="0.15" height="0.15")
+            path(d="M 5 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 25 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 45 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 65 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 85 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 105 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+        rect(width="4000" height="4000" fill="url(#sqiggle)")
+
+      svg.absolute.inset-0.transform.text-gray-200(class="-rotate-15 translate-x-1-24 translate-y-1-27 scale-75" width="4000" height="4000" fill="none")
+        defs
+          pattern(id="sqiggle" x="0" y="0" width="0.15" height="0.15")
+            path(d="M 5 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 25 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 45 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 65 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 85 15 c 0 -15 20 -15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
+            path(d="M 105 14.5 c 0 15 20 15 20 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="6")
         rect(width="4000" height="4000" fill="url(#sqiggle)")
 
   .relative.pt-6.pb-12(class="sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32")
