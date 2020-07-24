9799d26703387309824555db4a51bc96f3568501
Nick Gronow <nick@wov.io>
2020-07-24 08:57:40 -0400

    Add text-to-give

---
+++ src/components/HeroSplit.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+div(class="lg:relative")
+  .mx-auto.max-w-7xl.w-full.pt-16.pb-20.text-center(class="lg:py-48 lg:text-left")
+    .px-4(class="lg:w-1/2 sm:px-8 xl:pr-16")
+      h2.text-4xl.tracking-tight.leading-10.font-extrabold.text-gray-900(class="sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl")
+        span Data to enrich your
+        br(class="xl:hidden")
+        span.text-indigo-600 online business
+      p.mt-3.max-w-md.mx-auto.text-lg.text-gray-500(class="sm:text-xl md:mt-5 md:max-w-3xl") Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+  .relative.w-full.h-64(class="sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full")
+    img.absolute.inset-0.w-full.h-full.object-cover(src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="Woman on her phone")
+</template>
+
+<script>
+export default {
+  props: {
+    bg: {
+      type: String,
+      required: false,
+      default: 'primary'
+    }
+  },
+  computed: {
+    outerCss () {
+      if (this.bg === 'primary') {
+        return 'bg-gray-200'
+      } else if (this.bg === 'plain') {
+        return 'bg-white'
+      } else {
+        return 'bg-fg-dark'
+      }
+    },
+    textCss () {
+      if (this.bg === 'dark') {
+        return 'text-gray-100'
+      } else {
+        return 'text-fg-dark'
+      }
+    },
+    showPattern () {
+      return this.bg === 'primary'
+    }
+  }
+}
+</script>
--- src/pages/Give.vue
+++ src/pages/Give.vue
@@ -4,22 +4,29 @@ Layout
   g-image(src="~/assets/tithes-offerings.jpg")
 
   base-hero(bg="dark")
-    template(v-slot:heading) Thank you for supporting FloodGate
+    template(v-slot:heading) Give Online
 
     p.my-4.max-w-md.mx-auto.text-lg.text-gray-500(class="sm:my-6 sm:text-xl md:my-8 md:text-2xl lg:my-10 lg:max-w-3xl") Whether you wish to make a one-time gift or set up an automated giving schedule, our online giving system is a secure, convenient option. Think of it as a virtual offering bucket; a way that you can give to FloodGate from wherever you are.
 
-    base-button(@click.native="giveOnline") Give Online
+    base-button(@click.native="giveOnline")
+      span.mx-auto Give Now
 
   base-hero(bg="plain")
     template(v-slot:heading) Text to give
 
-    p.my-4.max-w-md.mx-auto.text-lg.text-gray-500(class="sm:my-6 sm:text-xl md:my-8 md:text-2xl lg:my-10 lg:max-w-3xl") Text any amount to 84321 to give
+    p.my-4.max-w-md.mx-auto.text-lg.text-gray-600(class="sm:my-6 sm:text-xl md:my-8 md:text-2xl lg:my-10 lg:max-w-3xl") Text any amount to 84321 to give
 
-    p.my-4.max-w-md.mx-auto.text-lg.text-gray-500(class="sm:my-6 sm:text-xl md:my-8 md:text-2xl lg:my-10 lg:max-w-3xl") Then follow a simple self-guided setup right from your phone. Setup takes about two minutes, tops. After that, donating is as simple as sending us a text. It’s the easiest & quickest way to donate!
+    iframe.w-full.my-8.mx-auto(class="sm:w-140 sm:h-80 md:my-12 md:w-160 md:h-90 lg:w-180 lg-h-100 lg:my-16 xl:w-200 xl:h-112" src="https://www.youtube.com/embed/JMwFYuqxlRg" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen")
+
+    p.my-4.max-w-md.mx-auto.text-lg.text-gray-600(class="sm:my-6 sm:text-xl md:my-8 md:text-2xl lg:my-10 lg:max-w-3xl") Then follow a simple self-guided setup right from your phone. Setup takes about two minutes, tops. After that, donating is as simple as sending us a text.
 </template>
 
 <script>
+import HeroSplit from '@/components/HeroSplit.vue'
 export default {
+  components: {
+    HeroSplit,
+  },
   metaInfo: {
     title: 'FloodGate Church - About Us',
     meta: [
