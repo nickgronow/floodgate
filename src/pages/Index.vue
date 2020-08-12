ad4b0a21758f786d677df510925f893857546d13
Nick Gronow <nick@wov.io>
2020-08-11 20:16:41 -0400

    Add squiggles

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,6 +6,13 @@ Layout
       svg.absolute.bottom-0.transform.-translate-y-64.translate-x-1-110.rotate-180.text-fg-blue.origin-center(class="sm:scale-125 sm:rotate-190 sm:translate-y-1-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 205")
         path(d="M 0 5 c 5 -5 15 -5 20 0 c 5 5 15 5 20 0 c 5 -5 15 -5 20 0 c 5 5 15 5 20 0 c 5 -5 15 -5 20 0 c 5 5 15 5 20 0 v 200 h -120 v -200" fill="currentColor" stroke="none")
 
+      .hidden(class="sm:block")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 top-10 left-30" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 top-10 right-10" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 top-50 left-10" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 bottom-40 left-40" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 bottom-40 right-10" src="shapes/squiggle.svg")
+
       main.relative.mt-10.mx-auto.max-w-screen-xl.text-center(class="sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28")
         p.text-yellow-600.uppercase.text-3xl.font-bold.-ml-40.gotham.tracking-widest.leading-none(class="sm:text-2xl sm:-ml-32 md:text-3xl md:-ml-40 lg:text-4xl lg:-ml-72") 10am
         h1.mx-auto.uppercase.text-6xl.tracking-widest.leading-none.text-fg-blue.gotham-bold.text-stroke-white(class="sm:text-5xl md:text-6xl lg:text-8xl xl:text-9xl") Sunday
@@ -15,6 +22,13 @@ Layout
     .relative.w-full.h-64.overflow-hidden.bg-fg-blue(class="sm:h-72 sm:absolute sm:inset-y-0 sm:right-0 sm:w-1/2 md:h-96 lg:h-128")
       svg.absolute.bottom-0.transform.-translate-y-64.translate-x-1-10.rotate-180.text-fg-dark.origin-center(class="sm:scale-125 sm:rotate-170 sm:translate-y-1-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 205")
         path(d="M 0 5 c 5 5 15 5 20 0 c 5 -5 15 -5 20 0 c 5 5 15 5 20 0 c 5 -5 15 -5 20 0 c 5 5 15 5 20 0 c 5 -5 15 -5 20 0 v 200 h -120 v -200" fill="currentColor" stroke="none")
+
+      .hidden(class="sm:block")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 top-10 left-30" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 top-10 right-10" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 top-50 left-10" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 bottom-40 left-40" src="shapes/squiggle.svg")
+        img.absolute.text-white.w-8(class="sm:w-8 md:w-12 bottom-40 right-10" src="shapes/squiggle.svg")
 
       main.relative.mt-10.mx-auto.max-w-screen-xl.text-center(class="sm:mt-12 md:mt-16 lg:mt-20 xl:mt-28")
         p.text-yellow-600.uppercase.text-3xl.font-bold.-ml-40.gotham.tracking-widest.leading-none(class="sm:text-2xl sm:-ml-32 md:text-3xl md:-ml-40 lg:text-4xl lg:-ml-72") 7pm
